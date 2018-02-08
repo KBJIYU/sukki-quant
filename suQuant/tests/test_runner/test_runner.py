0ef08fc350dba6e54c74f93bc62fd10e295fbab7
ffe <fatfingererr@gmail.com>
2018-02-08 22:40:57 +0800

    原本想測 loadcsv ... 時間不夠,今天就到此為止了 _(:3 」∠ )_

---
+++ suQuant/tests/test_runner/test_runner.py
@@ -0,0 +1,20 @@
+# TODO 未來 CI 跑測試...
+# 可能也可以在上層 test_main 跑..
+# 再思考看看
+# python -m unittest suQuant\tests\test_runner\test_runner.py
+
+from suQuant.runner.runner import Runner
+
+import unittest
+import datetime
+
+class TestRunner(unittest.TestCase):
+
+    aRunner = Runner()
+
+    def test_loadData(self):
+        now = datetime.datetime.now()
+        print(now)
+
+if __name__ == '__main__':
+    unittest.main()
